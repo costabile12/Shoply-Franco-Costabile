3adc815ddf38ceb7b511f8c9ed43d4d8bca8bdc1
costabile12 <franco_costabile@hotmail.com>
2025-11-23 15:36:24 -0300

    Actualice el navbar para que se contraiga cuando se cambia de link en movile

--- src/components/Navegation.jsx
+++ src/components/Navegation.jsx
@@ -16,6 +16,7 @@ export const Navegation = () => {
     const navigate = useNavigate();
     const { token, role, logout } = useAuthContext();
     const [busqueda, setBusqueda] = useState("");
+    const [expanded, setExpanded] = useState(false);
 
     const handleLogout = () => {
         logout();
@@ -27,9 +28,7 @@ export const Navegation = () => {
             showConfirmButton: false,
         });
         navigate("/login")
-        
-        
-
+        setExpanded(false);
     }
 
     const handleSearch = (e) => {
@@ -37,15 +36,16 @@ export const Navegation = () => {
         if (busqueda.trim() !== "") {
             navigate(`/buscar?query=${encodeURIComponent(busqueda)}`);
             setBusqueda("");
+            setExpanded(false);
         }
     }
 
     return(
         <>
-        <Navbar bg="light" expand="lg" className="p-1" >
+        <Navbar bg="light" expand="lg" className="p-1" expanded={expanded}  onToggle={(val) => setExpanded(val)}>
             <Container >
 
-                <Navbar.Brand as={Link} to="/" className="d-flex align-items-center gap-2 mx-3 mb-1">
+                <Navbar.Brand as={Link} to="/" className="d-flex align-items-center gap-2 mx-3 mb-1" onClick={() => setExpanded(false)}>
                 <img src={Logo} alt="Logo" width={55} height={55} />
                 <h2 className="title-responsive-nav fw-semibold mb-0 ">SHOPLY</h2>
                 </Navbar.Brand>
@@ -54,22 +54,22 @@ export const Navegation = () => {
                 <Navbar.Toggle />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto fw-semibold gap-3 ms-auto mb-3">
-                    <Nav.Link as={Link} to="/">Home</Nav.Link>
+                    <Nav.Link as={Link} to="/" onClick={()=>setExpanded(false)}>Home</Nav.Link>
                     <NavDropdown title="Shop" id="basic-nav-dropdown" >
-                        <NavDropdown.Item as={Link} to="/mens-clothing">Men's clothing</NavDropdown.Item>
-                        <NavDropdown.Item as={Link} to="/womens-clothing">Women's clothing</NavDropdown.Item>
-                        <NavDropdown.Item as={Link} to="/jewelery">Jewelery</NavDropdown.Item>
-                        <NavDropdown.Item as={Link} to="/electronics">Electronics</NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to="/mens-clothing" onClick={()=>setExpanded(false)}>Men's clothing</NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to="/womens-clothing" onClick={()=>setExpanded(false)}>Women's clothing</NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to="/jewelery" onClick={()=>setExpanded(false)}>Jewelery</NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to="/electronics" onClick={()=>setExpanded(false)}>Electronics</NavDropdown.Item>
                         <NavDropdown.Divider />
-                        <NavDropdown.Item as={Link} to="/offers" className="fw-bold">Offers</NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to="/offers" className="fw-bold" onClick={()=>setExpanded(false)}>Offers</NavDropdown.Item>
 
                     </NavDropdown>
-                    <Nav.Link as={Link} to="/about">About</Nav.Link>
-                    <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+                    <Nav.Link as={Link} to="/about" onClick={()=>setExpanded(false)}>About</Nav.Link>
+                    <Nav.Link as={Link} to="/contact" onClick={()=>setExpanded(false)}>Contact</Nav.Link>
 
                     {/* Solo se muestra si esta autenticado */}
                     {role === "admin" && (
-                        <Nav.Link as={Link} to="/admin" >Admin</Nav.Link>
+                        <Nav.Link as={Link} to="/admin" onClick={()=>setExpanded(false)}>Admin</Nav.Link>
                     ) }
                     
                 </Nav>
@@ -92,8 +92,8 @@ export const Navegation = () => {
                     <Carrito />
 
                    {!token ? (
-                    <Link  to="/login">
-                        <Button  type="button" className="btn btn-dark ms-2" aria-label="Login" title="Login">
+                    <Link  to="/login" onClick={()=>setExpanded(false)}>
+                        <Button  type="button" className="btn btn-dark ms-2" aria-label="Login" title="Login" >
                             <FontAwesomeIcon icon={faUser} style={{color: "#fafcff"}} />
 
                         </Button>
