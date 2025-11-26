abd6f9d07ee082dc1eb6283f16af4843a341685b
costabile12 <franco_costabile@hotmail.com>
2025-11-26 17:30:56 -0300

    Cambie la paginacion para que sea con bootstrap

--- src/components/Paginacion.jsx
+++ src/components/Paginacion.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
-
+import { Pagination } from "react-bootstrap";
 
 export const Paginacion = ({ totalPages, paginaActual, onPageChange }) => {
 
@@ -11,48 +10,135 @@ export const Paginacion = ({ totalPages, paginaActual, onPageChange }) => {
         onPageChange(page);
     };
 
+    const page1 = paginaActual - 2;
+    const page2 = paginaActual - 1;
+    const page3 = paginaActual + 1;
+    const page4 = paginaActual + 2;
+
     return (
-        <div className="d-flex justify-content-center align-items-center my-4 gap-2">
+    <>
+    {/* PAGINACION PARA ESCRITORIO */}
+        <Pagination className="justify-content-center my-3 d-none d-md-flex">
+            {/* Volver al la primera pagina */}
+            <Pagination.First
+            disabled={paginaActual === 1}
+            onClick={() => cambiarPagina(1)} />
 
-            {/* Botón Anterior */}
-            <button
-                className="btn btn-outline-primary"
-                onClick={() => cambiarPagina(paginaActual - 1)}
-                disabled={paginaActual === 1}
-            >
-                <FaArrowLeft />
-            </button>
+            {/* Ir a la pagina anterior de la actual */}
+            <Pagination.Prev 
+            disabled={paginaActual === 1}
+            onClick={() => cambiarPagina(paginaActual-1)}/>
 
-            {/* Vista de ESCRITORIO: Mostrar TODAS las páginas  */}
-            <div className="d-none d-md-flex">
-                {Array.from({ length: totalPages }, (_, index) => (
-                    <button
-                        key={index + 1}
-                        className={`btn mx-1 ${
-                            paginaActual === index + 1
-                                ? "btn-dark"
-                                : "btn-outline-dark"
-                        }`}
-                        onClick={() => cambiarPagina(index + 1)}
-                    >
-                        {index + 1}
-                    </button>
-                ))}
-            </div>
+            <Pagination.Item
+            active={paginaActual === 1}
+            onClick={() => cambiarPagina(1)}>
+                1
+            </Pagination.Item>
 
-            {/* Vista MÓVIL: Mostrar solo número actual */}
-            <span className="d-md-none fw-bold px-3 ">
-                <button className="btn mx-1 border-dark fw-bold">{paginaActual}</button> - <button className="btn mx-1 border-dark fw-bold">{totalPages}</button> 
-            </span>
+            {/* Ellipsis de izquierda */}
+            {paginaActual > 4 && (<Pagination.Ellipsis disabled/>)}
 
-            {/* Botón Siguiente */}
-            <button
-                className="btn btn-outline-primary"
-                onClick={() => cambiarPagina(paginaActual + 1)}
-                disabled={paginaActual === totalPages}
-            >
-                <FaArrowRight />
-            </button>
-        </div>
+            {/* paginaActual - 2 */}
+            {page1 > 1 && page1 < totalPages && (
+                <Pagination.Item 
+                onClick={()=>cambiarPagina(page1)}>
+                    {page1}
+                </Pagination.Item>
+            )}
+            
+            {/* paginaActual - 1 */}
+            {page2 > 1 && page2 < totalPages && (
+                <Pagination.Item onClick={()=>cambiarPagina(page2)}>
+                    {page2}
+                </Pagination.Item>
+            )}
+        
+            {/* paginaActual */}
+            {paginaActual !== 1 && paginaActual !== totalPages && (
+                <Pagination.Item active>
+                    {paginaActual}
+                </Pagination.Item>
+            )}
+            
+            {/* paginaActual + 1 */}
+            {page3 > 1 && page3 < totalPages && (
+                <Pagination.Item onClick={()=>cambiarPagina(page3)}>
+                    {page3}
+                </Pagination.Item>
+            ) }
+            
+            {/* paginaActual + 2 */}
+            {page4 > 1 && page4 < totalPages && (
+                <Pagination.Item onClick={()=>cambiarPagina(page4)}>
+                    {page4}
+                </Pagination.Item>
+            )}
+        
+            {/* Ellipsis de derecha */}
+            {paginaActual < totalPages - 3 && (
+                <Pagination.Ellipsis disabled />
+            )}
+
+            {/* Ultima pagina */}
+            {totalPages !== 1  && (
+                <Pagination.Item
+                active={paginaActual === totalPages}
+                onClick={() => cambiarPagina(totalPages)}>
+                    {totalPages}
+                </Pagination.Item>
+            )}
+            
+            {/*Ir a la pagina siguiente */}
+            <Pagination.Next 
+            disabled={paginaActual === totalPages}
+            onClick={() => cambiarPagina(paginaActual+1)}/>
+            
+            {/* Ir a la ultima pagina */}
+            <Pagination.Last 
+            disabled={paginaActual === totalPages}
+            onClick={()=>cambiarPagina(totalPages)}/>
+        </Pagination>
+
+        {/* PAGINACION PARA MOVILES */}
+        <Pagination className="justify-content-center my-3 d-flex d-md-none">
+            {/* Volver al la primera pagina */}
+            <Pagination.First
+            disabled={paginaActual === 1}
+            onClick={() => cambiarPagina(1)} />
+
+            {/* Ir a la pagina anterior de la actual */}
+            <Pagination.Prev 
+            disabled={paginaActual === 1}
+            onClick={() => cambiarPagina(paginaActual-1)}/>
+
+            {/* paginaActual */}
+            <Pagination.Item >
+                {paginaActual}
+            </Pagination.Item>
+            
+            <Pagination.Item >
+                /
+            </Pagination.Item>
+            
+            
+            {/* Cantidad de paginas */}
+            <Pagination.Item>
+                {totalPages}
+            </Pagination.Item>
+
+            {/*Ir a la pagina siguiente */}
+            <Pagination.Next 
+            disabled={paginaActual === totalPages}
+            onClick={() => cambiarPagina(paginaActual+1)}/>
+            
+            {/* Ir a la ultima pagina */}
+            <Pagination.Last 
+            disabled={paginaActual === totalPages}
+            onClick={()=>cambiarPagina(totalPages)}/>
+
+
+        </Pagination >
+    </>
+
     );
 };
